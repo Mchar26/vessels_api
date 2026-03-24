3d204269f7030bf9a44dac2c96b6fc8f3fab78cf
Manos Charitos <mcharitos@vesselbot.com>
2026-03-24 11:01:39 +0200

    add render config

--- Django/settings.py
+++ Django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-km2xip8fw4j*+=36_3e0-t5u22j-96iv=q2_p5-mov6%3!m*rk'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-km2xip8fw4j*+=36_3e0-t5u22j-96iv=q2_p5-mov6%3!m*r')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
